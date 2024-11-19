58ea51a8bc5cd717baf0be583377be68590375c4
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-11-19 17:05:35 +0700

    Bài 5

---
+++ Bài 5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	int a=5; //chieu dai
+	int b=2; //chieu rong
+	
+	int C=(a+b)*2;
+	int S=a*b;
+	
+	printf("Chu vi la: %d\n",C);
+	printf("Dien tich la: %d\n",S);
+	
+	return 0;
+}
