6f7654195b643460d3c30d7ee651e7cf73ab33c2
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-11-19 15:48:53 +0700

    Bài 1

---
+++ Bài 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main () {
+	int amount=50;//int:Luu tru so nguyen
+	
+	float pi = 3.14;//float:luu tru so co dau thap phan
+	
+	char letter = 'A';//char:luu tru mot ky tu duy nhat
+	
+	
+}
