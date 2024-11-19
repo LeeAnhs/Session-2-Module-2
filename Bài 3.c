1a70458a3faa50a30336c80c0dffdc6e39a0e83a
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-11-19 16:47:27 +0700

    Bài 3

---
+++ Bài 3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	int a=2;
+	int b=3;
+	int result1=a+b;
+	int result2=a/b;
+	int result3=a*b;
+	
+	printf("Tong hai so la: %d\n",result1);
+ 	printf("Thuong hai so la: %d\n", result2);
+	printf("Hieu hai so la: %d\n", result3);	
+	return 0;
+}
